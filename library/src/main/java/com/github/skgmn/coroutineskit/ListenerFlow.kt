cf3f9a0b696d25adb2ccabf40cedc6897c9fd63d
skgmn <suckgamony@gmail.com>
2021-09-26 13:33:01 +0900

    adjust default values

--- library/src/main/java/com/github/skgmn/coroutineskit/ListenerFlow.kt
+++ library/src/main/java/com/github/skgmn/coroutineskit/ListenerFlow.kt
@@ -1,7 +1,7 @@
 package com.github.skgmn.coroutineskit
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.buffer
@@ -11,6 +11,8 @@ import kotlin.coroutines.CoroutineContext
 
 @OptIn(ExperimentalCoroutinesApi::class)
 fun <T> listenerFlow(
+    extraBufferCapacity: Int = 256,
+    onBufferOverflow: BufferOverflow = BufferOverflow.DROP_OLDEST,
     context: CoroutineContext? = null,
     block: ListenerFlowCollector<T>.() -> Unit
 ): Flow<T> {
@@ -29,5 +31,5 @@ fun <T> listenerFlow(
         awaitClose { onClose?.invoke() }
     }.run {
         context?.let { flowOn(it) } ?: this
-    }.buffer(Channel.UNLIMITED)
+    }.buffer(extraBufferCapacity, onBufferOverflow)
 }
