f5b59973267b01ebe8f36fc2c236eceffd62385b
skgmn <suckgamony@gmail.com>
2021-10-02 22:03:30 +0900

    add chunked

---
+++ library/src/main/java/com/github/skgmn/coroutineskit/Chunked.kt
@@ -0,0 +1,92 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.github.skgmn.coroutineskit
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+
+fun <T> Flow<T>.chunked(count: Int): Flow<List<T>> {
+    return flow {
+        val buffer = mutableListOf<T>()
+        collect {
+            buffer += it
+            if (buffer.size >= count) {
+                val list = buffer.toList()
+                buffer.clear()
+                emit(list)
+            }
+        }
+        if (buffer.isNotEmpty()) {
+            emit(buffer)
+        }
+    }
+}
+
+fun <T> Flow<T>.chunked(timeMillis: Long): Flow<List<T>> {
+    return channelFlow {
+        coroutineScope {
+            val buffer = mutableListOf<T>()
+            val timerJob = launch {
+                while (isActive) {
+                    delay(timeMillis)
+                    val list = synchronized(buffer) {
+                        buffer.toList().also { buffer.clear() }
+                    }
+                    send(list)
+                }
+            }
+            try {
+                collect {
+                    synchronized(buffer) { buffer += it }
+                }
+                timerJob.cancel()
+                val finalList = synchronized(buffer) { buffer.toList() }
+                if (finalList.isNotEmpty()) {
+                    send(finalList)
+                }
+            } finally {
+                timerJob.cancel()
+            }
+        }
+    }
+}
+
+fun <T> Flow<T>.chunked(count: Int, timeMillis: Long): Flow<List<T>> {
+    return channelFlow {
+        coroutineScope {
+            val buffer = mutableListOf<T>()
+            val timerJob = launch {
+                while (isActive) {
+                    delay(timeMillis)
+                    val list = synchronized(buffer) {
+                        buffer.toList().also { buffer.clear() }
+                    }
+                    send(list)
+                }
+            }
+            try {
+                collect {
+                    val list = synchronized(buffer) {
+                        buffer += it
+                        if (buffer.size >= count) {
+                            buffer.toList().also { buffer.clear() }
+                        } else {
+                            null
+                        }
+                    }
+                    list?.let { send(it) }
+                }
+                timerJob.cancel()
+                val finalList = synchronized(buffer) { buffer.toList() }
+                if (finalList.isNotEmpty()) {
+                    send(finalList)
+                }
+            } finally {
+                timerJob.cancel()
+            }
+        }
+    }
+}
